b5108eed47bf279030df2f87f7b85f303a3458b6
Darma Lopez <ldarma112@gmail.com>
2026-03-23 16:04:35 -0600

    change

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const mongodb =require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getALL = async (requestAnimationFrame, res) => {
-    const result = await mongodb.getDatabase().bd().collection('users').find();
+    const result = await mongodb.getDatabase().db().collection('users').find();
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users);
